2ee71d21bcedfc115a52a07963a37a32452be2d7
rssc <github@ione.ch>
2014-09-16 20:12:51 +0200

    Removed garbage characters.

--- php/database/update_020100_pgsql.php
+++ php/database/update_020100_pgsql.php
@@ -9,7 +9,7 @@
 $result = $database->query("SELECT tags FROM ".LYCHEE_TABLE_PHOTOS." LIMIT 1");
 if($result === FALSE) {
 	$result = $database->query("ALTER TABLE ".LYCHEE_TABLE_PHOTOS." ADD tags CHARACTER VARYING(1000) DEFAULT ''");
-	if ($result === FALSE) {
+	if ($result === FALSE) {
 		Log::error($database, 'update_020100', __LINE__, 'Could not update database (' . print_r($database->errorInfo(), TRUE) . ')');
 		return false;
 	}
@@ -18,7 +18,7 @@ if($result === FALSE) {
 $result = $database->query("SELECT key FROM ".LYCHEE_TABLE_SETTINGS." WHERE key = 'dropboxKey' LIMIT 1");
 if ($result->rowCount()===0) {
 	$result = $database->query($database, "INSERT INTO ".LYCHEE_TABLE_SETTINGS." (key, value) VALUES ('dropboxKey', '')");
-	if ($result === FALSE) {
+	if ($result === FALSE) {
 		Log::error($database, 'update_020100', __LINE__, 'Could not update database (' . print_r($database->errorInfo(), TRUE) . ')');
 		return false;
 	}
--- php/database/update_020200_mysql.php
+++ php/database/update_020200_mysql.php
@@ -9,7 +9,7 @@
 $result = $database->query("SELECT `visible` FROM `".LYCHEE_TABLE_ALBUMS."` LIMIT 1");
 if($result === FALSE) {
 	$result = $database->query("ALTER TABLE `".LYCHEE_TABLE_ALBUMS."` ADD `visible` TINYINT(1) NOT NULL DEFAULT 1");
-	if ($result === FALSE) {
+	if ($result === FALSE) {
 		Log::error($database, 'update_020200', __LINE__, 'Could not update database (' . print_r($database->errorInfo(), TRUE) . ')');
 		return false;
 	}
